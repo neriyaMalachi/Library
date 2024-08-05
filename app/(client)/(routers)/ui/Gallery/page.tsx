1ea266b7996ab01613eb8190a0e31d013fb8e30d
Neriya Malachi <neriamalachi@gmail.com>
2024-08-05 15:16:27 +0300

    fear
    finish to add swiper in laybrary

--- app/(client)/(routers)/ui/Gallery/page.tsx
+++ app/(client)/(routers)/ui/Gallery/page.tsx
@@ -1,27 +1,59 @@
+'use client'
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import image1 from '@/assets/ImageForZoomFile.png'
+import image2 from '@/assets/logo.png'
+import image3 from '@/assets/logoWithBackround.jpg'
+import image4 from '@/assets/bg-main-mobile.png'
+import image5 from '@/assets/bg-card-front.png'
+import image6 from '@/assets/icon-complete.svg'
+import Image from "next/image";
 
-// Import Swiper styles
-import "swiper/css";
-import { Navigation, Pagination } from "swiper/modules";
+
 
 const page = () => {
   return (
-    <Swiper
-      navigation
-      pagination={{ type: "fraction" }}
-      modules={[Navigation, Pagination]}
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+      <section className="w-[90%] border border-solid p-2 flex flex-col justify-center text-center">
+       <h1 className="text-xl font-bold">Swiper Images</h1>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className=" rounded-lg text-black "
+        >
+          <SwiperSlide>
+            <Image src={image1} alt="image1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={image2} alt="image2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={image3} alt="image3" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={image4} alt="image4" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={image5} alt="image5" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={image6} alt="image6" />
+          </SwiperSlide>
+        </Swiper>
+      </section>
   );
 };
 
